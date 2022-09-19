896e09b97f0de54e37ee32f83aab71fe4fbafc1b
Eugene <Eugene.Nisnevich@Gmail.com>
2022-09-19 17:18:29 -0400

    Add project files.

---
+++ CoffeeShopRegistrationLab/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using CoffeeShopRegistrationLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopRegistrationLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private CoffeeShopRegistrationLabContext context = new CoffeeShopRegistrationLabContext();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        public IActionResult AddUserToDb(User user)
+        {
+            context.Users.Add(user);
+            context.SaveChanges();
+            return RedirectToAction("GetUser", user);
+        }
+
+        public IActionResult GetUser()
+        {
+            List<User> users = context.Users.ToList();
+            return View(users);
+        }
+        
+    }
+}
---
+++ CoffeeShopRegistrationLab/Models/CoffeeShopRegistrationLabContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CoffeeShopRegistrationLab.Models
+{
+    public partial class CoffeeShopRegistrationLabContext : DbContext
+    {
+        public CoffeeShopRegistrationLabContext()
+        {
+        }
+
+        public CoffeeShopRegistrationLabContext(DbContextOptions<CoffeeShopRegistrationLabContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-423LBI0;Initial Catalog=CoffeeShopRegistrationLab; Integrated Security=SSPI;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Email).HasMaxLength(30);
+
+                entity.Property(e => e.FirstName).HasMaxLength(30);
+
+                entity.Property(e => e.LastName).HasMaxLength(30);
+
+                entity.Property(e => e.Password).HasMaxLength(30);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
